4a394300fa2d99970273173c870ecb998286a49f
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-07 16:39:05 -0500

    Added User.js model

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        // make username and email lower case to ensure consistant data
+        newUserData.username = await newUserData.username.toLowerCase();
+        newUserData.email = await newUserData.email.toLowerCase();
+
+        // encrypt password
+        return newUserData;
+      },
+
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.email = await updatedUserData.email.toLowerCase();
+        newUserData.email = await newUserData.email.toLowerCase();
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
