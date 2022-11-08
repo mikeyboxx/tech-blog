516b0cd4117347057563243ec157589de47ceb8d
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-07 19:04:26 -0500

    Added Comment.js model and updated index.js with associations to the User and Post tables

--- models/index.js
+++ models/index.js
@@ -2,23 +2,31 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-
+// User has a one to many relationship with Post
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
-
 Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
+// User has a one to many relationship with Comment
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
-
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
+});
+
+// Post has a one to many relationship with Comment
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
 });
 
 module.exports = { User, Post };
