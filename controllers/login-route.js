e82374ac83f0333012edf0bb01602cb310e01c83
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-08 17:33:37 -0500

    Create login-route.js to handle login

---
+++ controllers/login-route.js
@@ -0,0 +1,11 @@
+// Login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  // Otherwise, render the 'login' template
+  res.render('login');
+});
